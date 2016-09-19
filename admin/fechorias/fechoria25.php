4142485818b01f3569275d56dbd64b2f95661991
Rubén López Herrera <ruben@iMac-de-Ruben.local>
2016-09-19 16:12:08 +0200

    Solución error al registrar problemas de convivencia

--- admin/fechorias/fechoria25.php
+++ admin/fechorias/fechoria25.php
@@ -151,7 +151,7 @@ for ($i=0;$i<$num_a;$i++){
 	 	 
 	 	 $mail->msgHTML($message);
 	 	 $mail->Subject = $config['centro_denominacion'].' - Comunicación de Problemas de Convivencia';
-	 	 $mail->AltBody = 'Jefatura de Estudios le comunica que, con fecha '.$fecha.', su hijo ha cometido una falta '.$grave.' contra las normas de convivencia del Centro. El tipo de falta es el siguiente: '.$asunto.'.<br>Le recordamos que puede conseguir información más detallada en la página del alumno de nuestra web en http://'.$config['dominio'].', o bien contactando con la Jefatura de Estudios del Centro.<br><br><hr>Este correo es informativo. Por favor, no responder a esta dirección de correo. Si necesita mayor información sobre el contenido de este mensaje, póngase en contacto con Jefatura de Estudios.'
+	 	 $mail->AltBody = 'Jefatura de Estudios le comunica que, con fecha '.$fecha.', su hijo ha cometido una falta '.$grave.' contra las normas de convivencia del Centro. El tipo de falta es el siguiente: '.$asunto.'.<br>Le recordamos que puede conseguir información más detallada en la página del alumno de nuestra web en http://'.$config['dominio'].', o bien contactando con la Jefatura de Estudios del Centro.<br><br><hr>Este correo es informativo. Por favor, no responder a esta dirección de correo. Si necesita mayor información sobre el contenido de este mensaje, póngase en contacto con Jefatura de Estudios.';
 
 	 	 $mail->AddAddress($correo, $nombre_alumno);
 	 	 $mail->Send();	
