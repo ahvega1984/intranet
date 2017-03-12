46d8c203ae3c2203adf8d7b18b2812ff342cf362
Rubén López <ruben@iMac-de-Ruben.local>
2017-03-12 21:39:08 +0100

    Update .gitignore

--- admin/fechorias/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// CONFIGURACIÓN MÓDULO DE PROBLEMAS DE CONVIVENCIA
-$config['convivencia']['notificaciones_padres']	= 1;
-$config['convivencia']['mostrar_descripcion']	= 0;
-
-
-// Fin del archivo de configuración
