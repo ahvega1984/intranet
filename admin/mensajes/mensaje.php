b72b93d7472e410f3f236bebfd923523e6ba028d
Rubén López <ruben@iMac-de-Ruben.local>
2017-09-09 22:56:26 +0200

    Actualizado tareas y noticias

--- admin/mensajes/mensaje.php
+++ admin/mensajes/mensaje.php
@@ -12,13 +12,29 @@ else {
 	$id = intval($_GET['id']);
 }
 
-
 $result = mysqli_query($db_con, "SELECT mens_texto.asunto, mens_texto.ahora, mens_texto.texto, mens_texto.origen FROM mens_texto JOIN mens_profes ON mens_texto.id = mens_profes.id_texto WHERE mens_texto.id = '$id' LIMIT 1") or die (mysqli_error($db_con));
 $mensaje = mysqli_fetch_array($result);
 
 if(mysqli_num_rows($result)<1) {
 	header('Location:'.'index.php');
 	exit();
+}
+
+// Consultamos si el mensaje se ha marcado como tarea
+$result_tarea = mysqli_query($db_con, "SELECT id FROM tareas WHERE titulo = '".$mensaje['asunto']."' AND tarea = '".$mensaje['texto']."'");
+if (mysqli_num_rows($result_tarea)) {
+	$esTarea = 1;
+}
+else {
+	$esTarea = 0;
+
+	// Marcar como tarea
+	if (isset($_GET['tarea']) && $_GET['tarea']) {
+		$titulo = $mensaje['asunto'];
+		$tarea = htmlspecialchars_decode($mensaje['texto']);
+		$fechareg = date('Y-m-d H:i:s');
+		mysqli_query($db_con, "INSERT tareas (idea, titulo, tarea, estado, fechareg, prioridad) VALUES ('".$idea."', '".$titulo."', '".$tarea."', 0, '".$fechareg."', 0)");	
+	}
 }
 
 include("../../menu.php");
@@ -57,9 +73,12 @@ include("menu.php");
 	      <div class="hidden-print">
 	      	<a href="index.php" class="btn btn-default">Volver</a>
 	      	<a href="redactar.php?profes=1&amp;origen=<?php echo $mensaje['origen']; ?>&amp;asunto=RE: <?php echo $mensaje['asunto']; ?>" class="btn btn-primary">Responder</a>
-	      	<a href="#" class="btn btn-info" onclick="javascript:print();">Imprimir</a>
+					<a href="#" class="btn btn-info" onclick="javascript:print();">Imprimir</a>
+					<?php if (! $esTarea): ?>
+					<a href="mensaje.php?id=<?php echo $id; ?>&amp;idprof=<?php echo $idprof; ?>&amp;tarea=1" class="btn btn-warning">Marcar como tarea</a>
+					<?php endif; ?>
 	      	<?php $id !== $idprof ? $buzon='recibidos' : $buzon='enviados'; ?>
-			<a href="index.php?inbox=<?php echo $buzon; ?>&amp;delete=<?php echo $idprof; ?>" class="btn btn-danger" data-bb="confirm-delete">Eliminar</a>
+					<a href="index.php?inbox=<?php echo $buzon; ?>&amp;delete=<?php echo $idprof; ?>" class="btn btn-danger" data-bb="confirm-delete">Eliminar</a>
 	      </div>
 	      
 	    </div><!-- /.col-sm-12 -->
