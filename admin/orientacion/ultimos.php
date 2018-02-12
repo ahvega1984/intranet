96f3071ebcaf38d440252595b2c278da17cb2fd1
rubenlh91 <rubenlh91@gmail.com>
2018-02-12 14:50:22 +0100

    Registro de intervenciones sobre grupos

--- admin/orientacion/ultimos.php
+++ admin/orientacion/ultimos.php
@@ -7,26 +7,34 @@ $trozos = explode (", ", $al);
 $apellidos = $trozos[0];
 $nombre = $trozos[1];
 
-$result0 = mysqli_query($db_con, "select distinct apellidos, nombre, claveal from tutoria where orienta = '1' order by fecha desc");
-//echo "select distinct apellidos, nombre, claveal from tutoria where orienta = '1' order by fecha asc";
-$n_filas = mysqli_num_rows($result0);
-if($n_filas > 0) 
-  {
-echo '<table class="table table-striped table-bordered datatable">';
-  	
-echo "<thead><tr><th>Alumno</th><th>Fecha</th></tr></thead><tbody>";
-  while($alumn = mysqli_fetch_array($result0))
-  {
- $clave=$alumn[2];
-    $result = mysqli_query($db_con, "select distinct apellidos, nombre, fecha, accion, causa, observaciones, unidad, tutor, id, prohibido from tutoria where orienta = '1' and claveal = '$clave' order by fecha desc limit 1");
-while($row = mysqli_fetch_array($result))
-{
-$id3 = $row[8];
-$prohibido = $row[9];
-echo "<tr><td><a href='tutor.php?id=$id3'>$row[1] $row[0]</a></div></td><td nowrap>$row[2]</td></tr>";
-}	
-  }
-	echo "</tbody></table>";
-}
+?>
 
-  ?>
+<?php $result = mysqli_query($db_con, "SELECT DISTINCT apellidos, nombre, claveal FROM tutoria WHERE orienta = '1' ORDER BY fecha DESC"); ?>
+<?php if(mysqli_num_rows($result) > 0): ?>
+<table class="table table-striped table-bordered datatable">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>Alumno</th>
+			<th>Fecha</th>
+		</tr>
+	</thead>
+	<tbody>
+  	<?php while ($row = mysqli_fetch_array($result)): ?>
+    <?php $result_alumno = mysqli_query($db_con, "SELECT DISTINCT id, apellidos, nombre, fecha, unidad FROM tutoria WHERE orienta = '1' AND claveal = '".$row['claveal']."' ORDER BY fecha DESC LIMIT 1"); ?>
+	<?php while($row_alumno = mysqli_fetch_array($result_alumno)): ?>
+		<tr>
+			<td><?php echo $row_alumno['id']; ?></td>
+			<td>
+				<a href="index.php?id=<?php echo $row_alumno['id']; ?>">
+					<span class="pull-right badge badge-default"><?php echo $row_alumno['unidad']; ?></span>
+					<?php echo ($row_alumno['apellidos'] == 'Todos') ? 'Todos los alumnos' : $row_alumno['nombre'].' '.$row_alumno['apellidos']; ?>
+				</a>
+				</td>
+			<td nowrap><?php echo $row_alumno['fecha']; ?></td>
+		</tr>
+	<?php endwhile; ?>
+	<?php endwhile; ?>
+	</tbody>
+</table>
+<?php endif; ?>
