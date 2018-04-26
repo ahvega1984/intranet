e592a9eb57b5c8cf1ef948f91f6d0897a4115d96
mgarcia39 <mgarcia39@gmail.com>
2018-04-26 12:45:43 +0200

    Solución de errores

--- admin/noticias/config.php
+++ admin/noticias/config.php
@@ -2,7 +2,7 @@
 
 // CONFIGURACIÓN MÓDULO DE NOTICIAS
 $config['noticias']['web_centro']	= 1;
-$cat = array("Dirección del Centro","Jefatura","Secretaría","Actividades Extraescolares","Proyecto Escuela de Paz","Centro Bilingue","Centro TIC","Ciclos Formativos");
+$cat = array("Dirección del Centro","Jefatura de Estudios","Secretaría","Actividades Extraescolares","Proyecto Escuela de Paz","Centro Bilingue","Centro TIC","Ciclos Formativos");
 
 
 // Fin del archivo de configuración
