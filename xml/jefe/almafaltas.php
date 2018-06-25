609d694f9de755bd9969df4d64c907474032e1e3
Rubén López <rubenlh91@gmail.com>
2018-06-25 10:09:56 +0200

    Modificación SQL importación de alumnos

--- xml/jefe/almafaltas.php
+++ xml/jefe/almafaltas.php
@@ -14,33 +14,33 @@ include '../../menu.php';
 	<div class="page-header">
 		<h2>Administración <small> Creación de la tabla de alumnos</small></h2>
 	</div>
-	
+
 	<div id="status-loading" class="text-center">
 		<span class="lead"><span class="fa fa-circle-o-notch fa-spin"></span> Cargando...</span>
 	</div>
 
 	<div id="wrap" class="row" style="display: none;">
-	
+
 		<div class="col-sm-8 col-sm-offset-2">
-	
+
 			<div class="well">
-			
+
 				<?php
 				if($archivo1 and $archivo2){
 					// Comprobamos si es la primera vez que se ha creado una base de datos.
 					$fechorias = mysqli_query($db_con, "select * from Fechoria");
 					$mensajes = mysqli_query($db_con, "select * from mens_texto limit 10");
 					$reg_int = mysqli_query($db_con, "select * from reg_intranet limit 10");
-			
+
 					if ((mysqli_num_rows($fechorias) > 5) && (mysqli_num_rows($mensajes) > 5) && (mysqli_num_rows($reg_int) > 5)) {
 						include("copia_bd.php");
 					}
-			
+
 			// Copia de Seguridad
 			mysqli_query($db_con, "DROP TABLE alma_seg") ;
 			mysqli_query($db_con, "create table alma_seg select * from alma");
 			mysqli_query($db_con,"drop table alma");
-			
+
 			// Creación de la tabla alma
 			$alumnos = "CREATE TABLE  `alma` (
 			`Alumno/a` varchar( 255 ) default NULL ,
@@ -81,10 +81,9 @@ include '../../menu.php';
 			 `NACIONALIDAD` varchar( 32 ) default NULL,
 			 `SEXO` varchar( 1 ) default NULL ,
 			 `FECHAMATRICULA` varchar( 255 ) default NULL,
-			 `SEGSOCIAL` CHAR(12) NULL,
-			 PRIMARY KEY (`CLAVEAL`)  
+			 PRIMARY KEY (`CLAVEAL`)
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
-			
+
 					// echo $alumnos;
 					mysqli_query($db_con, $alumnos) or die ('<div align="center"><div class="alert alert-danger alert-block fade in">
 			            <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -94,12 +93,12 @@ include '../../menu.php';
 			<div align="center">
 			  <input type="button" value="Volver atrás" name="boton" onClick="history.back(2)" class="btn btn-inverse" />
 			</div>');
-			
+
 					$SQL6 = "ALTER TABLE `alma` ADD PRIMARY KEY(`CLAVEAL`)";
 					$result6 = mysqli_query($db_con, $SQL6);
-			
+
 					// Importamos los datos del fichero CSV (todos_alumnos.csv) en la tabla alma.
-			
+
 					$fp = fopen ($_FILES['archivo1']['tmp_name'] , "r" ) or die('<div align="center"><div class="alert alert-danger alert-block fade in">
 			            <button type="button" class="close" data-dismiss="alert">&times;</button>
 						<h5>ATENCIÓN:</h5>
@@ -107,7 +106,7 @@ include '../../menu.php';
 			</div></div><br />
 			<div align="center">
 			  <input type="button" value="Volver atrás" name="boton" onClick="history.back(2)" class="btn btn-inverse" />
-			</div>'); 
+			</div>');
 
 					while (!feof($fp))
 					{
@@ -126,14 +125,14 @@ include '../../menu.php';
 						$n_col_tabla++;
 					}
 
-					if ($n_col_tabla!=$num_col) { 
+					if ($n_col_tabla!=$num_col) {
 
 						// Restauramos Copia de Seguridad porque Séneca ha modificado la estructura de RegAlum.txt
 						mysqli_query($db_con, "insert into alma select * from alma_seg");
 						echo '<br><div align="center"><div class="alert alert-danger alert-block fade in">
 			            <button type="button" class="close" data-dismiss="alert">&times;</button>
 						<h5>ATENCIÓN:</h5>
-						No se han podido importar los datos de los alumnos porque Séneca ha modificado la estructura del archivo RegAlum.txt, bien porque ha añadido algún campo bien porque lo ha eliminado. Ahora mismo el archivo tiene '.$num_col.' campos de datos mientras que la tabla tiene '.$n_col_tabla.' columnas. 
+						No se han podido importar los datos de los alumnos porque Séneca ha modificado la estructura del archivo RegAlum.txt, bien porque ha añadido algún campo bien porque lo ha eliminado. Ahora mismo el archivo tiene '.$num_col.' campos de datos mientras que la tabla tiene '.$n_col_tabla.' columnas.
 						<br>Se mantienen las tablas tal como estaban mientras actualizas la aplicación o lo comunicas a los desarrolladores para que estos puedan arreglar el asunto.
 						</div></div><br />
 						<div align="center">
@@ -141,15 +140,15 @@ include '../../menu.php';
 						</div>';
 						?>
 									</div><!-- /.well -->
-	
+
 							</div><!-- /.col-sm-8 -->
-							
+
 						</div><!-- /.row -->
-						
+
 					</div><!-- /.container -->
 
 					<?php include("../../pie.php");	?>
-					
+
 					<?php
 					exit();
 					}
@@ -157,16 +156,16 @@ include '../../menu.php';
 					while (!feof($fp))
 					{
 						$num_linea++;
-						
+
 						$linea="";
 						$lineasalto="";
 						$dato="";
 						$linea=fgets($fp);
-						
+
 						if ($num_linea > 8) {
 							$lineasalto = "INSERT INTO alma VALUES (";
 							$tr=explode("|",$linea);
-				
+
 							foreach ($tr as $valor){
 								$dato.= "\"". mysqli_real_escape_string($db_con, trim(utf8_encode($valor))) . "\", ";
 							}
@@ -177,16 +176,16 @@ include '../../menu.php';
 						}
 					}
 					fclose($fp);
-			
+
 					// Descomprimimos el zip de las calificaciones en el directorio exporta/
 					include('../../lib/pclzip.lib.php');
-					
-					// Borramos archivos antiguos					
-					$files = glob('../exporta/*'); 
+
+					// Borramos archivos antiguos
+					$files = glob('../exporta/*');
 						foreach($files as $file)
-						{ 
+						{
   						if(is_file($file) and stristr($file, "index")==FALSE)
-    						unlink($file); 
+    						unlink($file);
 						}
 
 					$archive = new PclZip($_FILES['archivo2']['tmp_name']);
@@ -194,7 +193,7 @@ include '../../menu.php';
 					{
 						die("Error : ".$archive->errorInfo(true));
 					}
-			
+
 					// Procesamos datos
 					$crear = "ALTER TABLE  alma
 			ADD  `COMBASI` VARCHAR( 250 ) NULL FIRST ,
@@ -203,7 +202,7 @@ include '../../menu.php';
 			ADD  `PADRE` VARCHAR( 78 ) NULL AFTER  `CLAVEAL1`
 			";
 					mysqli_query($db_con, $crear);
-			
+
 					// Apellidos unidos formando un solo campo.
 					$SQL2 = "SELECT apellido1, apellido2, CLAVEAL, NOMBRE FROM  alma";
 					$result2 = mysqli_query($db_con, $SQL2);
@@ -216,7 +215,7 @@ include '../../menu.php';
 						$actualiza1= "UPDATE alma SET APELLIDOS = \"". $apellidos1 . "\", NOMBRE = \"". $nombre1 . "\" where CLAVEAL = \"". $row2[2] . "\"";
 						mysqli_query($db_con, $actualiza1);
 					}
-			
+
 					// Apellidos y nombre del padre.
 					$SQL3 = "SELECT PRIMERAPELLIDOTUTOR, SEGUNDOAPELLIDOTUTOR, NOMBRETUTOR, CLAVEAL FROM  alma";
 					$result3 = mysqli_query($db_con, $SQL3);
@@ -227,32 +226,32 @@ include '../../menu.php';
 						$actualiza1P= "UPDATE alma SET PADRE = \"". $apellidos1P . "\" where CLAVEAL = \"". $row3[3] . "\"";
 						mysqli_query($db_con, $actualiza1P);
 					}
-			
+
 					// Eliminación de campos innecesarios por repetidos
 					$SQL3 = "ALTER TABLE alma
 			  DROP `apellido1`,
 			  DROP `Alumno/a`,
 			  DROP `apellido2`";
 					$result3 = mysqli_query($db_con, $SQL3);
-			
+
 					// Eliminación de alumnos dados de baja
 					$SQL4 = "DELETE FROM alma WHERE `unidad` = ''";
 					$result4 = mysqli_query($db_con, $SQL4);
-			
+
 					include("exportacodigos.php");
-			
+
 					// Eliminamos alumnos sin asignaturas que tienen la matricula pendiente, y que no pertenecen a los Ciclos
 					$SQL6 = "DELETE FROM alma WHERE (COMBASI IS NULL and (curso like '%E.S.O.%' or curso like '%Bach%' or curso like '%F.P.B%') and ESTADOMATRICULA not like 'Obtiene T%' and ESTADOMATRICULA not like 'Repit%' and ESTADOMATRICULA not like 'Promocion%' and ESTADOMATRICULA not like 'Pendiente de confirma%' and ESTADOMATRICULA not like 'Traslad%') or (COMBASI IS NULL and ESTADOMATRICULA like 'Traslad%')";
 					$result6 = mysqli_query($db_con, $SQL6);
-			
+
 					// Eliminamos a los alumnoos de Ciclos con algun dato en estadomatricula
 					//$SQL7 = "DELETE FROM alma WHERE ESTADOMATRICULA not like '' and ESTADOMATRICULA not like 'Obtiene T%' and ESTADOMATRICULA not like 'Repit%' and ESTADOMATRICULA not like 'Promocion%'  and ESTADOMATRICULA not like 'Pendiente de confirm%'";
 					//mysqli_query($db_con, $SQL7);
-			
+
 					// Creamos una asignatura ficticia para que los alumnos sin Asignaturas puedan aparecer en las listas
 					$SQL8 = "update alma set combasi = 'Sin_Asignaturas' where combasi IS NULL";
 					mysqli_query($db_con, $SQL8);
-			
+
 					// Creamos version corta para FALTAS
 					mysqli_query($db_con, "DROP TABLE almafaltas");
 					mysqli_query($db_con, "CREATE TABLE almafaltas select CLAVEAL, NOMBRE, APELLIDOS, unidad from alma") or die('<div align="center"><div class="alert alert-danger alert-block fade in">
@@ -263,30 +262,30 @@ include '../../menu.php';
 			<div align="center">
 			  <input type="button" value="Volver atrás" name="boton" onClick="history.back(2)" class="btn btn-inverse" />
 			</div>');
-			
+
 					// Claveal primaria e indice
 					$SQL6 = "ALTER TABLE  `almafaltas` ADD INDEX (  `CLAVEAL` )";
 					$result6 = mysqli_query($db_con, $SQL6);
-			
+
 					echo '<div class="alert alert-success alert-block fade in">
 			            <button type="button" class="close" data-dismiss="alert">&times;</button>
 			<h5>ALUMNOS DEL CENTRO:</h5> los Alumnos se han introducido correctamente en la Base de datos.
 			</div><br />';
-					
+
 					// Eliminamos temporales
 					mysqli_query($db_con, "drop table almafaltas");
-					
+
 					// Copia de la primera versión de alma
 					mysqli_query($db_con, "DROP TABLE alma_primera");
 					mysqli_query($db_con, "create table alma_primera select * from alma");
 					mysqli_query($db_con, "ALTER TABLE  `alma_primera` ADD INDEX (  `CLAVEAL` )");
-			
+
 					// Alumnos con hermanos
 					include("crear_hermanos.php");
 
 					// Asignaturas y alumnos con pendientes
 					include("asignaturas.php");
-			
+
 				}
 				else{
 					echo '<div align="center"><div class="alert alert-danger alert-block fade in">
@@ -296,17 +295,17 @@ include '../../menu.php';
 			</div></div><br />';
 				}
 				?>
-				
+
 				<div class="text-center">
 					 <a href="../index.php" class="btn btn-primary">Volver a Administración</a>
 				</div>
-			
+
 			</div><!-- /.well -->
-	
+
 		</div><!-- /.col-sm-8 -->
-		
+
 	</div><!-- /.row -->
-	
+
 </div><!-- /.container -->
 
 <?php include("../../pie.php");	?>
@@ -314,7 +313,7 @@ include '../../menu.php';
 <script>
 function espera() {
 	document.getElementById("wrap").style.display = '';
-	document.getElementById("status-loading").style.display = 'none';        
+	document.getElementById("status-loading").style.display = 'none';
 }
 window.onload = espera;
 </script>
