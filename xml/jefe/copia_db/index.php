ea298420142851193107ef556e73fa253295e093
Rubén López <rubenlh91@gmail.com>
2018-09-17 16:30:18 +0200

    Solución error icono descargar copia base de datos

--- xml/jefe/copia_db/index.php
+++ xml/jefe/copia_db/index.php
@@ -6,14 +6,14 @@ acl_acceso($_SESSION['cargo'], array(1));
 $profe = $_SESSION['profi'];
 
 function copia_bd($host, $user, $pass, $name) {
-   
+
    $backup_file = 'db-backup_'.$name.'_'.date('YmdHis').'.sql.gz';
-   
+
    $command = "mysqldump --opt --ignore-table=$name.fotos -h $host -u $user -p$pass $name | gzip -9 > $backup_file";
-    
+
    // ejecución y salida de éxito o errores
    system($command,$output);
-   return $output;   
+   return $output;
 }
 
 function mb_file($bytes) {
@@ -50,7 +50,7 @@ if(isset($_GET['action']) && $_GET['action']=="crear") {
 
 
 	$result = copia_bd($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
-	
+
 	if($result) {
 		$msg_error = "No ha sido posible crear la copia de seguridad. Asegúrese de que el directorio ".__DIR__." tiene permiso de escritura.";
 	}
@@ -76,27 +76,27 @@ include("../../../menu.php");
 ?>
 
 <div class="container">
-	
+
 	<div class="page-header">
 	  <h2>Administración <small> Copia de seguridad de la base de datos</small></h2>
 	</div>
-	
+
 	<?php if(isset($msg_success)): ?>
 	<div class="alert alert-success">
 		<?php echo $msg_success; ?>
 	</div>
 	<?php endif; ?>
-	
+
 	<?php if(isset($msg_error)): ?>
 	<div class="alert alert-danger">
 		<?php echo $msg_error; ?>
 	</div>
 	<?php endif; ?>
-	
+
 	<div class="row">
-		
+
 		<div class="col-sm-12">
-			
+
 			<div class="table-responsive">
 				<table class="table table-striped">
 					<thead>
@@ -117,7 +117,7 @@ include("../../../menu.php");
 							<td><?php echo date("d-m-Y H:i:s", filectime($archivo)); ?></td>
 							<td nowrap>
 								<a href="restaurar.php?archivo=<?php echo $archivo; ?>" data-bs="tooltip" title="Restaurar"><span class="far fa-undo fa-lg fa-fw"></span></a>
-								<a href="index.php?action=descargar&archivo=<?php echo $archivo; ?>" data-bs="tooltip" title="Descargar"><span class="far fa-cloud-download fa-lg fa-fw"></span></a>
+								<a href="index.php?action=descargar&archivo=<?php echo $archivo; ?>" data-bs="tooltip" title="Descargar"><span class="fas fa-download fa-lg fa-fw"></span></a>
 								<a href="index.php?action=eliminar&archivo=<?php echo $archivo; ?>" data-bb="confirm-delete" data-bs="tooltip" title="Eliminar"><span class="far fa-trash-alt fa-lg fa-fw"></span></a>
 							</td>
 						</tr>
@@ -126,15 +126,15 @@ include("../../../menu.php");
 					<?php endwhile; ?>
 				</table>
 			</div>
-			
+
 			<a class="btn btn-primary" href="index.php?action=crear">Crear copia</a>
 			<a class="btn btn-default" href="restaurar.php">Restaurar desde archivo</a>
 			<a class="btn btn-default" href="../../index.php">Volver</a>
-			
+
 		</div>
-		
+
 	</div>
-	
+
 </div>
 
 <?php include("../../../pie.php"); ?>
