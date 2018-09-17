50e5d47b707baa9966279129ece56783454a75df
Rubén López <rubenlh91@gmail.com>
2018-09-17 18:26:37 +0200

    Corrección consulta SQL selección alumnos

--- admin/cursos/seleccion_alumnos.php
+++ admin/cursos/seleccion_alumnos.php
@@ -19,13 +19,13 @@ if (isset($_POST['guardar_cambios'])) {
         if ($asignatura != $asignatura_ant || $unidad != $unidad_ant) {
             // Saltamos este bloque la primera vez que se ejecuta foreach o si key corresponde al botón de envío de formulario
             if ($flag_primera_vez > 1 || $key != 'guardar_cambios') {
-                
+
                 $result_alumnos = mysqli_query($db_con, "SELECT alma.apellidos, alma.nombre, alma.claveal FROM alma WHERE alma.unidad = '".$unidad_ant."' AND alma.combasi LIKE '%".$asignatura_ant."%' ORDER BY alma.apellidos ASC, alma.nombre ASC");
                 $total_alumnos = mysqli_num_rows($result_alumnos);
                 $alumnos_seleccionados = count($array_nc);
-                
+
                 if ($total_alumnos != $alumnos_seleccionados) {
-                    
+
                     $nc_separado_por_comas = implode(",", $array_nc);
 
                     // Comprobamos si el profesor ya seleccionó alumnos. En ese caso actualizamos los datos, si no, insertamos en la tabla
@@ -49,7 +49,7 @@ if (isset($_POST['guardar_cambios'])) {
                         mysqli_query($db_con, "DELETE FROM grupos WHERE id = '$id_seleccion'");
                     }
                 }
-                
+
             }
 
             $array_nc = array();
@@ -78,7 +78,7 @@ include("../../menu.php");
 ?>
 
     <div class="container">
-        
+
         <!-- TITULO DE LA PAGINA -->
         <div class="page-header">
             <h2>Mi alumnado <small>Selección de alumnos por materias</small></h2>
@@ -92,11 +92,11 @@ include("../../menu.php");
                     <p>Desmarca aquellos alumnos a los que no impartes materia. Esto evitará que aparezca las notificaciones de Informes de tareas o tutoría, o aparezca en tus listados de grupos y faltas de asistencia.</p>
                 </div>
 
-                <?php $result = mysqli_query($db_con, "SELECT DISTINCT p.nivel, p.materia, p.grupo, m.codigo FROM profesores AS p, materias AS m WHERE p.profesor = '".$pr."' AND p.materia = m.nombre AND p.grupo = m.grupo AND m.abrev NOT LIKE '%\_%'"); ?>
+                <?php $result = mysqli_query($db_con, "SELECT DISTINCT p.nivel, p.materia, p.grupo, m.codigo FROM profesores AS p, materias AS m WHERE p.profesor = '".$pr."' AND p.materia = m.nombre AND p.grupo = m.grupo AND p.nivel = m.curso AND m.abrev NOT LIKE '%\_%'"); ?>
                 <?php if (mysqli_num_rows($result)): ?>
                 <form action="" method="post">
                     <div class="panel-group" id="materias" role="tablist" aria-multiselectable="true">
-                    
+
                         <?php $i = 0; ?>
                         <?php while ($row = mysqli_fetch_array($result)): ?>
 
@@ -122,15 +122,15 @@ include("../../menu.php");
                             <div class="panel-heading" role="tab" id="heading<?php echo $i; ?>">
                                 <h4 class="panel-title">
                                     <a role="button" data-toggle="collapse" data-parent="#materias" href="#collapse<?php echo $i; ?>" aria-expanded="true" aria-controls="collapse<?php echo $i; ?>" style="display: block;">
-                                    <?php echo $row['grupo'].' - '.$row['materia'].' <span class="text-muted pull-right">'.$total_alumnos_seleccionados.'/'.$total_alumnos_unidad.' <span class="hidden-xs">alumnos</span></span>'; ?>
+                                    <?php echo $row['grupo'].' <small>('.$row['nivel'].')</small> - '.$row['materia'].' <span class="text-muted pull-right">'.$total_alumnos_seleccionados.'/'.$total_alumnos_unidad.' <span class="hidden-xs">alumnos</span></span>'; ?>
                                     </a>
                                 </h4>
                             </div>
                             <div id="collapse<?php echo $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $i; ?>">
                                 <div class="panel-body">
-                                    
+
                                     <?php while ($row_alumno = mysqli_fetch_array($result_alumnos)): ?>
-                                    <?php 
+                                    <?php
                                     $nombre_checkbox = 'checkbox_'.$row['codigo'].'_'.$row['grupo'].'_'.$row_alumno['claveal'];
                                     if (mysqli_num_rows($result_alumnos_seleccionados) > 0 && in_array($row_alumno['claveal'], $nc_alumnos_seleccionados)) {
                                         $checkbox_checked = "checked";
@@ -153,13 +153,13 @@ include("../../menu.php");
                         </div>
                         <?php $i++; ?>
                         <?php endwhile; ?>
-            
+
                     </div>
 
                     <button type="submit" class="btn btn-primary" name="guardar_cambios">Guardar cambios</button>
                     <button type="submit" class="btn btn-default" name="restablecer_seleccion">Restablecer selección</button>
                 </form>
-                
+
                 <?php else: ?>
 
                 <div class="well text-center">
@@ -170,9 +170,9 @@ include("../../menu.php");
 
                 <?php endif; ?>
             </div>
-            
+
         </div><!-- /.row -->
-        
+
     </div><!-- /.container -->
 
 	<?php include("../../pie.php"); ?>
