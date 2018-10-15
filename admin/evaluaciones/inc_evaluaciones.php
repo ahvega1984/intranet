7e867893dbb50316ef2cab9ecc0567d2cf330d1c
Rubén López <rubenlh91@gmail.com>
2018-10-15 11:44:34 +0200

    Corrección nombre de evaluaciones

--- admin/evaluaciones/inc_evaluaciones.php
+++ admin/evaluaciones/inc_evaluaciones.php
@@ -1,13 +1,13 @@
 <?php defined('INTRANET_DIRECTORY') OR exit('No direct script access allowed');
 
 $evaluaciones = array(
-    'EVI' => 'Evaluación inicial',
+  'EVI' => 'Evaluación inicial',
 	'1EV' => '1ª Evaluación',
 	'2EV' => '2ª Evaluación',
 	'3EV' => '3ª Evaluación',
 	'ORD' => 'Ordinaria',
-    'FFP' => 'Final FP',
+  'FFP' => 'Final FP',
 	'EXT' => 'Extraordinaria',
-	'EP1' => 'Evaluación de pendientes 1ª Convovatoria',
-	'EP2' => 'Evaluación de pendientes 2ª Convovatoria'
+	'EP1' => 'Evaluación de pendientes 1ª Convocatoria',
+	'EP2' => 'Evaluación de pendientes 2ª Convocatoria'
 );
