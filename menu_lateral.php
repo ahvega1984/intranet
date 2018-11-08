747887653b3ed2a8add6dd6ddcef875a8f8c3177
mgarcia39 <mgarcia39@gmail.com>
2018-11-08 09:41:31 +0100

    Solución de errores

--- menu_lateral.php
+++ menu_lateral.php
@@ -671,7 +671,7 @@ if ($_SERVER['SERVER_NAME'] == 'iesmonterroso.org') {
 					'target' => '_blank',
 				),
 				array(
-					'href'   => 'https://iesmonterroso.org/plan-anual-de-centro//',
+					'href'   => 'https://iesmonterroso.org/plan-de-centro/',
 					'titulo' => 'Plan de centro',
 					'target' => '_blank',
 				),
