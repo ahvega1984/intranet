5977e39fbcb208b58318609c7a432a87d956d581
mgarcia39 <mgarcia39@gmail.com>
2018-12-16 19:27:05 +0100

    Enlace del módulo de Aulas libres con el sistema de reservas

--- admin/cursos/aulas_libres.php
+++ admin/cursos/aulas_libres.php
@@ -61,6 +61,10 @@ include("../../menu.php");
 								$dia_sem = $dia-1;
 								$nuevafecha = strtotime ( '+'.$dia_sem.' day' , strtotime ( $primer_dia ) ) ;
 								$fecha_dia = date ( 'Y-m-d' , $nuevafecha );
+								$tr_fecha = explode("-", $fecha_dia);
+								$anio0 = $tr_fecha[0];
+								$mes0 = $tr_fecha[1];
+								$dia0 = $tr_fecha[2];
 								?>
 								<?php $result = mysqli_query($db_con, "SELECT DISTINCT a_aula, n_aula FROM horw WHERE c_asig NOT LIKE '25' AND a_aula NOT LIKE '' AND a_aula NOT LIKE 'ACO%' AND a_aula NOT LIKE 'DI%' ORDER BY n_aula ASC"); ?>
 							<?php while ($row = mysqli_fetch_array($result)): ?>
@@ -71,7 +75,7 @@ include("../../menu.php");
 							<?php $res = mysqli_query($db_con,"select * from reservas where date(eventdate) = '$fecha_dia' and event".$i." not like '' and servicio = '$row[0]'");
 							$ya_reserva = mysqli_num_rows($res);?>
 							<?php if($asig['a_grupo'] == '' and $ya_reserva != 1): ?>
-							<p><a href="hor_aulas.php?aula=<?php echo $row['n_aula']; ?>"><?php echo $row['n_aula']; ?></a></p>
+							<p><a href="../../reservas/reservar/index_aulas.php?year=<?php echo $anio0;?>&today=<?php echo $dia0;?>&month=<?php echo $mes0;?>&servicio=<?php echo $row['n_aula']; ?>"><?php echo $row['n_aula']; ?></a></p>
 							<?php endif; ?> <?php endwhile; ?></td>
 							<?php endfor; ?>
 						</tr>
