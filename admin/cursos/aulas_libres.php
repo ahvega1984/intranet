d309dd836f819e7509e9d80ad4b94fc87e07e65b
mgarcia39 <mgarcia39@gmail.com>
2018-12-17 07:32:57 +0100

    Solución de errores

--- admin/cursos/aulas_libres.php
+++ admin/cursos/aulas_libres.php
@@ -12,6 +12,9 @@ if ($n_dia == 'Jueves') {	$dia = '4';}
 if ($n_dia == 'Viernes') {	$dia = '5';}
 
 include("../../menu.php");
+
+$week = date('W');
+$year = date('Y');
 ?>
 
 <div class="container">
@@ -20,11 +23,17 @@ include("../../menu.php");
 
 		<h2 style="display: inline;"><?php echo $n_dia; ?> <small>Consulta de aulas libres</small></h2>
 
-		<?php $dias = array('Lunes','Martes','Miércoles','Jueves','Viernes'); ?>
+		<?php $dias = array('Lunes','Martes','Miércoles','Jueves','Viernes'); ?>		
 		<form class="pull-right col-sm-2" method="post" action="">
 			<select class="form-control" id="n_dia" name="n_dia" onChange="submit()">
 				<?php for($i = 0; $i < count($dias); $i++): ?>
-				<option value="<?php echo $dias[$i]; ?>" <?php echo ($dias[$i] == $n_dia) ? 'selected' : ''; ?>><?php echo $dias[$i]; ?></option>
+				<?php
+				$primer_dia0 = date('Y-m-d', strtotime($year . 'W' . str_pad($week , 2, '0', STR_PAD_LEFT)));
+				$dia_sem0 = $i;
+				$nuevafecha0 = strtotime ( '+'.$dia_sem0.' day' , strtotime ( $primer_dia0 ) ) ;
+				$fecha_dia0 = date ( 'Y-m-d' , $nuevafecha0 );
+				?>	
+				<option value="<?php echo $dias[$i]; ?>" <?php echo ($dias[$i] == $n_dia) ? 'selected' : ''; ?>><?php echo $dias[$i]." ".cambia_fecha($fecha_dia0).""; ?></option>
 				<?php endfor; ?>
 			</select>
 		</form>
@@ -55,8 +64,6 @@ include("../../menu.php");
 						<?php for($i = 1; $i < $count_cols; $i++): ?>
 							<td>
 								<?php
-								$week = date('W');
-								$year = date('Y');
 								$primer_dia = date('Y-m-d', strtotime($year . 'W' . str_pad($week , 2, '0', STR_PAD_LEFT)));
 								$dia_sem = $dia-1;
 								$nuevafecha = strtotime ( '+'.$dia_sem.' day' , strtotime ( $primer_dia ) ) ;
@@ -75,7 +82,7 @@ include("../../menu.php");
 							<?php $res = mysqli_query($db_con,"select * from reservas where date(eventdate) = '$fecha_dia' and event".$i." not like '' and servicio = '$row[0]'");
 							$ya_reserva = mysqli_num_rows($res);?>
 							<?php if($asig['a_grupo'] == '' and $ya_reserva != 1): ?>
-							<p><a href="../../reservas/reservar/index_aulas.php?year=<?php echo $anio0;?>&today=<?php echo $dia0;?>&month=<?php echo $mes0;?>&servicio=<?php echo $row['n_aula']; ?>"><?php echo $row['n_aula']; ?></a></p>
+							<p><a href="../../reservas/reservar/index_aulas.php?year=<?php echo $anio0;?>&today=<?php echo $dia0;?>&month=<?php echo $mes0;?>&servicio=<?php echo $row[0]; ?>"><?php echo $row['n_aula']; ?></a></p>
 							<?php endif; ?> <?php endwhile; ?></td>
 							<?php endfor; ?>
 						</tr>
