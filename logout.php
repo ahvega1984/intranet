1e82814bf576ea03d6477e723aec3398f40ddd9b
rubenlh91 <rubenlh91@gmail.com>
2019-12-07 15:30:05 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ require('bootstrap.php');
 session_unset();
 session_destroy();
 
-if ($_SERVER['SERVER_NAME'] == "iesmonterroso.org") {
+if ($_SERVER['SERVER_NAME'] == "iesmonterroso.org" || date('Y') >= '2020') {
 	header("Location://".$config['dominio']."/intranet/loginSeneca.php");
 	exit();
 }
