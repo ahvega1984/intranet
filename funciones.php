794816e92fe10d19c45120224e3b4e395c1cfe3c
rubenlh91 <rubenlh91@gmail.com>
2020-05-03 21:05:19 +0200

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -54,32 +54,32 @@ function limpiarInput($input, $type = 'alphanumeric') {
 		
 		// ALLOW MAYUS
 		case 'mayus':
-			$output = preg_replace('([^A-ZÁÉÍÓÚÑºª])', '', $input);
+			$output = preg_replace('([^A-ZÁÉÍÓÜÑºª])', '', $input);
 
 			break;
 
 		// ALLOW MINUS
 		case 'minus':
-			$output = preg_replace('([^a-záéíóúñ])', '', $input);
+			$output = preg_replace('([^a-záéíóúüñ])', '', $input);
 
 			break;
 
 		// ALLOW LETTERS (MAYUS AND MINUS)
 		case 'alpha':
-			$output = preg_replace('([^A-ZÁÉÍÓÑÚa-záéíóúñ])', '', $input);
+			$output = preg_replace('([^A-ZÁÉÍÓÑÚÜa-záéíóúüñ])', '', $input);
 
 			break;
 
 		// ALLOW ALPHANUMERIC
 		case 'alphanumeric':
-			$output = preg_replace('([^A-ZÁÉÍÓÚÑa-záéíóúñ0-9])', '', $input);
+			$output = preg_replace('([^A-ZÁÉÍÓÚÜÑa-záéíóúüñ0-9])', '', $input);
 
 			break;
 
 		// ALLOW ALPHANUMERIC AND SPECIAL CHARS: space,  !"#$%&'()*+,-./:;»=>?@[\]^_`{|}~
 		case 'alphanumericspecial':
 		default:
-			$output = preg_replace('([^A-ZÁÉÍÓÚÑa-záéíóúñºª0-9 !"#$%&\'()*+,-./:;»=>?@[\]^_`{|}~])', '', $input);
+			$output = preg_replace('([^A-ZÁÉÍÓÚÜÑa-záéíóúüñºª0-9 !"#$%&\'()*+,-./:;»=>?@[\]^_`{|}~])', '', $input);
 
 			break;
 
