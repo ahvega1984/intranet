c10290d701945f1291ae3e115454d04d812f0cd7
mgarcia39 <miguel@iesmonterroso.org>
2020-10-21 19:59:45 +0200

    Solución de error en bachilleratos mixtos

--- xml/jefe/pendientes.php
+++ xml/jefe/pendientes.php
@@ -1,5 +1,6 @@
 <?php defined('INTRANET_DIRECTORY') OR exit('No direct script access allowed');
 
+// Regeneración de la tabla de pendientes
 mysqli_query($db_con, "drop TABLE pendientes");
 mysqli_query($db_con, "CREATE TABLE IF NOT EXISTS pendientes (
   id int(11) NOT NULL auto_increment,
@@ -13,21 +14,22 @@ mysqli_query($db_con, "CREATE TABLE IF NOT EXISTS pendientes (
 
 $cur = mysqli_query($db_con,"select claveal, combasi, unidad, curso from alma where curso not like '1%'");
 while ($uni = mysqli_fetch_array($cur)) {
-	$claveal = $uni[0];
-	$combasi = $uni[1];
-	$unidad = $uni[2];
-	$curso = $uni[3];
-	
+  $claveal = $uni[0];
+  $combasi = $uni[1];
+  $unidad = $uni[2];
+  $curso = $uni[3];
+  
 $trozos1 = explode(":", $combasi);
  foreach ($trozos1 as $asig)
   {
-$nombreasig = "select NOMBRE, ABREV, CURSO, CODIGO from asignaturas where CODIGO = '" . $asig . "' and curso = '$curso' and abrev like '%\_%'";
+$curso_r = substr($curso,0,12);
+$nombreasig = "select NOMBRE, ABREV, CURSO, CODIGO from asignaturas where CODIGO = '" . $asig . "' and curso like '$curso_r%' and abrev like '%\_%'";
 $asig2 = mysqli_query($db_con, $nombreasig);
 if (mysqli_num_rows($asig2)>0) {
-	$cod = "INSERT INTO pendientes (claveal, codigo, grupo) VALUES ('$claveal', '$asig', '$unidad')";	
-	mysqli_query($db_con, $cod);
+  $cod = "INSERT INTO pendientes (claveal, codigo, grupo) VALUES ('$claveal', '$asig', '$unidad')";
+  mysqli_query($db_con, $cod);
 }
-  }	
+  } 
 }
 ?>
 <div class="alert alert-success alert-block fade in">
@@ -35,4 +37,4 @@ if (mysqli_num_rows($asig2)>0) {
             <h5>ALUMNOS CON ASIGNATURAS PENDIENTES:</h5>
 Los alumnos con asignaturas pendientes han sido importados en la base de datos. Ya es posible realizar consultas y ver listados de pendientes por Grupo o Asignatura (Menú de la página principal ==> Consultas ==> Listados ==> Listas de Pendientes).
 </div>
- <br />
+
