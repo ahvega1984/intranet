81fdd95e53544f7e1e61721ba3872f12eb3ee00c
mgarcia39 <miguel@iesmonterroso.org>
2021-02-03 08:35:19 +0100

    Archivo innecesario

--- admin/noticias/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// CONFIGURACIÓN MÓDULO DE NOTICIAS
-$config['noticias']['web_centro']	= 1;
-$cat = array("Dirección del Centro","Jefatura de Estudios","Secretaría","Actividades Extraescolares","Proyecto Escuela de Paz","Centro Bilingue","Centro TIC","Ciclos Formativos");
-
-
-// Fin del archivo de configuración
